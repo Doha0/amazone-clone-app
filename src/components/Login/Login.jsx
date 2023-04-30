6231b224c304681a813e5295cddc6821d5219f8d
Doha0 <abu.jardoha@gmail.com>
2023-04-30 11:04:47 +0600

    login and signup page added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <p>Login coming soon.....</p>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold mb-4">Login to Ama-Zone!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
 
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="custom-button">Login</button>
+                        </div>
+                        <label className="label">
+                            <Link to='/sign-up' className="label-text-alt link link-hover">New to Ama-Zone? Create An Account</Link>
+                        </label>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Sign Up coming soon</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold mb-4">Sign Up to Ama-zone</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input type="password" name='confirm' placeholder="confirm password" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="custom-button">Sign UP</button>
+                        </div>
+                        <label className="label">
+                            <Link to='/login' className=" label-text-alt link link-hover">Already have an account? Login</Link>
+                        </label>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
